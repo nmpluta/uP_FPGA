22c58e210e1a79dd3e0538d5f7a73d13814106d3
IAmNotAProgramer <dawidsce@gmail.com>
2022-01-23 20:31:08 +0100

    internal reset in one module

--- project/run.tcl
+++ project/run.tcl
@@ -31,6 +31,7 @@ proc attach_rtl_files {} {
                 rtl/program.vhd
                 rtl/alu_decode.vhd
                 rtl/flags.vhd
+                rtl/state_control.vhd
                 rtl/strobe_enables_decode.vhd
                 rtl/program_counter_decode.vhd
                 rtl/program_counter.vhd
